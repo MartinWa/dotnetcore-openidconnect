662a1d715ea7df446d6d4fda945b4ee18bfc9714
Martin Wåger <martin.wager@comaround.com>
2019-05-04 18:36:30 +0200

    Working

---
+++ IdpSettings.cs
@@ -0,0 +1,9 @@
+namespace sample
+{
+    public class IdpSettings
+    {
+        public string Authority { get; set; }
+        public string ClientId { get; set; }
+        public string CustomScope { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var idpSettings = Configuration.GetSection("Idp").Get<IdpSettings>();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect(options =>
+                {
+                    options.Authority = idpSettings.Authority ;
+                    options.ClientId = idpSettings.ClientId;
+                    options.ResponseType = "id_token";
+                    options.Scope.Clear();
+                    options.Scope.Add("openid");
+                    options.Scope.Add(idpSettings.CustomScope);
+                    options.Events.OnRemoteFailure = RemoteAuthFail;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                    options.SaveTokens = true;
+
+                });
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+        }
+
+        private Task RemoteAuthFail(RemoteFailureContext arg)
+        {
+            return Task.CompletedTask;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            //     app.UseCookiePolicy(); // Enabling this causes validation errors in /signin-oicd 
+
+            app.UseMvc();
+        }
+    }
+}
